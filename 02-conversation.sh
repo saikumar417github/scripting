8e87f2d852dd387d4aab9d2bca8049c1610e614d
sai <sai@gmail.com>
2025-07-25 18:00:00 +0530

    second commit

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hi Suresh, How are you?"
+echo "Suresh: Hi Ramesh, I am fine, what about you?"
+echo "Ramesh: that's great Suresh, catch you later"
+echo "Suresh: ok Ramesh"
