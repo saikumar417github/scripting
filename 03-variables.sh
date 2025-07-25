6271b3271b632a95241e84f82896ce6180ebef40
sai <sai@gmail.com>
2025-07-25 18:03:34 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hi $PERSON2, How are you?"
+echo "$PERSON2: Hi $PERSON1, I am fine, what about you?"
+echo "$PERSON1: that's great $PERSON2, catch you later"
+echo "$PERSON2: ok $PERSON1"
