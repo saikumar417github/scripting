2e8085996c2d7d2870b9725c915c5817362b7f4b
sai <sai@gmail.com>
2025-07-25 18:09:35 +0530

    arguments

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "${PERSON1}: Hi $PERSON2, How are you?"
+echo "$PERSON2: Hi $PERSON1, I am fine, what about you?"
+echo "$PERSON1: that's great $PERSON2, catch you later"
+echo "$PERSON2: ok $PERSON1"
