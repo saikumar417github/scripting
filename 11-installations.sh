d7f2cab2f067ed65c2dd48f129070635faa76249
sai <sai@gmail.com>
2025-07-29 19:50:23 +0530

    installation

--- 11-installations.sh
+++ 11-installations.sh
@@ -12,10 +12,15 @@ fi
 
 yum list installed git
 
-if [ echo $? -gt 0 ]
+if [ $? -ne 0 ]
 then 
 echo "Git is not installed, goting to install it"
 yum install git -y
+if [ $? -ne 0 ]
+then
+echo "Git installation is failed, please check"
+else
+echo "Git installation is successful"
 else
 echo "Git is already installed nothing to do"
 fi
