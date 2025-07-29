322fee4ea49e8629ebc807b901ddbe0f21b8e82f
sai <sai@gmail.com>
2025-07-29 19:45:11 +0530

    installation

--- 11-installations.sh
+++ 11-installations.sh
@@ -10,4 +10,12 @@ echo "Please run script with root user"
 exit 1
 fi
 
+yum list installed git
+
+if [ echo $? -gt 0 ]
+then 
+echo "Git is not installed, goting to install it"
 yum install git -y
+else
+echo "Git is already installed nothing to do"
+fi
